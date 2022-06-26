6d96a494b5dbdd498c3552f182d035e75d47b1a2
ljk <jiakun.ui@gmail.com>
2022-06-27 00:11:15 +0800

    feat: add meta obj in route meta data to provide arguments

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,46 @@
+import 'vue-router';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import Home from '@/views/Home.vue';
 import NotFound from '@/views/NotFound.vue';
 
+declare module 'vue-router' {
+  interface RouteMeta {
+    title: string;
+  }
+}
+
 const history = createWebHistory();
 export default createRouter({
   history,
   routes: [
-    { path: '/', component: Home },
-    { path: '/404', component: NotFound },
-    { path: '/:pathMatch(.*)', component: NotFound },
+    {
+      path: '/',
+      component: Home,
+      redirect: '/sub',
+      children: [
+        {
+          path: '/sub',
+          component: Home,
+          meta: {
+            title: 'sub',
+          },
+        },
+      ],
+    },
+    {
+      path: '/404',
+      component: NotFound,
+      meta: {
+        title: 'notFound',
+      },
+    },
+    {
+      path: '/:pathMatch(.*)',
+      component: NotFound,
+      meta: {
+        title: 'notFound',
+      },
+    },
   ],
 });
