67173b0fb6056d5422f81a565a518159e351e46d
ljk <jiakun.ui@gmail.com>
2022-06-27 19:48:11 +0800

    fix(app-layout): fix page-body height over the app node

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,25 @@
 
 <script setup lang="ts">
   import NavBar from '@/components/NavBar.vue'
-  import { provide } from 'vue'
+  import { provide, computed } from 'vue'
+  import { useRoute } from 'vue-router'
 
   // 判断处于pwa时增加底部安全距离，提供给子组件
   type NavigatorExtend = Navigator & {
     standalone?: boolean
   }
   const navigator: NavigatorExtend = window.navigator
-  const tabBarSafeAreaBottom = navigator.standalone ? '32px' : '6px'
+  const tabBarSafeAreaBottom = navigator.standalone ? 32 : 6
   provide('tabBarSafeAreaBottom', tabBarSafeAreaBottom)
+
+  const route = useRoute()
+  const height = computed(() => {
+    if (route.meta.needTabBar) {
+      return 56 + 44 + 12 + tabBarSafeAreaBottom + 'px'
+    } else {
+      return '56px'
+    }
+  })
 </script>
 
 <style lang="scss">
@@ -29,7 +39,9 @@
     color: #303133;
 
     .page-body {
+      height: calc(100vh - v-bind(height));
       padding: 12px;
+      overflow: hidden;
     }
   }
 </style>
