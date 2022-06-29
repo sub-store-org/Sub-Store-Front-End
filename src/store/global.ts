646aa9052c5a52e981f106a235f81518e724370f
ljk <jiakun.ui@gmail.com>
2022-06-29 11:29:40 +0800

    refactor(app): fix the issue that data is lost when editing page is refreshed

--- src/store/global.ts
+++ src/store/global.ts
@@ -3,6 +3,8 @@ import { defineStore } from 'pinia';
 export const useGlobalStore = defineStore('globalStore', {
   state: (): GlobalStoreState => {
     return {
+      isLoading: false,
+      fetchResult: false,
       bottomSafeArea: 0,
     };
   },
@@ -10,6 +12,12 @@ export const useGlobalStore = defineStore('globalStore', {
   actions: {
     setBottomSafeArea(height: number) {
       this.bottomSafeArea = height;
+    },
+    setLoading(isLoading: boolean) {
+      this.isLoading = isLoading;
+    },
+    setFetchResult(fetchResult: boolean) {
+      this.fetchResult = fetchResult;
     },
   },
 });
