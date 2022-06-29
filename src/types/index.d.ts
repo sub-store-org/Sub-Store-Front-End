5548ffd0dc65d6e1ac01e078bdb61600f7ba465a
ljk <jiakun.ui@gmail.com>
2022-06-29 15:17:49 +0800

    refactor(fetch-all): Refactor the logic of entering the app to get data and add the notification switch

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,1 +1,11 @@
 type SubsType = 'sub' | 'collection';
+
+interface ErrorResponse {
+  status: 'failed';
+  error: {
+    code: string;
+    type: string;
+    message: string;
+    detail?: string;
+  };
+}
