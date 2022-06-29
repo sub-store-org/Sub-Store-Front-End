6e64616ea9aeb55a7c3440b3ce25e60229efad1d
ljk <jiakun.ui@gmail.com>
2022-06-29 15:20:31 +0800

    feat(editor): add editor route and add get one sub or collection methods

--- src/views/SubEditor.vue
+++ src/views/SubEditor.vue
@@ -1,10 +1,27 @@
-<template>{{ params }} 订阅编辑页</template>
+<template>{{ editType === 'sub' ? sub : collection }} 订阅编辑页</template>
 
 <script lang="ts" setup>
   import { useRoute } from 'vue-router'
+  import { useSubsStore } from '@/store/subs'
+  import { watchEffect, ref } from 'vue'
 
   const route = useRoute()
   const { params } = route
+  const { editType, id } = params
+  const sub = ref<SubsData>()
+  const collection = ref<CollectionsData>()
+
+  const subsStore = useSubsStore()
+  watchEffect(() => {
+    switch (editType) {
+      case 'sub':
+        sub.value = subsStore.getOneSub(id as string)
+        break
+      case 'collection':
+        collection.value = subsStore.getOneCollection(id as string)
+        break
+    }
+  })
 </script>
 
 <style lang="scss" scoped></style>
