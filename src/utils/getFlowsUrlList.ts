40b3b7081bf55b06c986d1d721fb097cc6d9875f
ljk <jiakun.ui@gmail.com>
2022-07-12 12:50:39 +0800

    fix(get-flow): Get flows changed to asynchronous, automatically refresh flow when adding remote sub

---
+++ src/utils/getFlowsUrlList.ts
@@ -0,0 +1,13 @@
+export const getFlowsUrlList = (subs: Sub[]): string[][] => {
+  const nameList = [];
+  const urlList = [];
+
+  subs.forEach(sub => {
+    if (!urlList.includes(sub.url) && sub.source === 'remote') {
+      urlList.push(sub.url);
+      nameList.push([sub.url, sub.name]);
+    }
+  });
+  console.log(nameList);
+  return nameList;
+};
