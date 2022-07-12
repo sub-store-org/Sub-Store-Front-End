7d827072346f6112a56779da178924cfb8152d3e
ljk <jiakun.ui@gmail.com>
2022-07-12 14:09:39 +0800

    fix(style): fix auto dark mode in safari below 13.0

--- src/utils/setColorThemeClass.ts
+++ src/utils/setColorThemeClass.ts
@@ -3,35 +3,26 @@ import { useGlobalStore } from '@/store/global';
 export const setColorThemeClass = () => {
   const themeList = ['light-mode', 'dark-mode'];
   const body = document.querySelector('body');
+  const mql = window.matchMedia('(prefers-color-scheme: dark)');
 
-  // 判断 safari 版本
-  const Sys = { safari: '' };
-  const ua = navigator.userAgent.toLowerCase();
-  let s;
-  (s = ua.match(/version\/([\d\.]+).*safari/)) ? (Sys.safari = s[1]) : 0;
-
-  // 不对 safari 13 及以下使用自动 dark-mode
-  if (Sys.safari >= '14.0' || Sys.safari === '') {
-    if (window?.matchMedia('(prefers-color-scheme: dark)').matches) {
+  const setTheme = () => {
+    if (mql.matches) {
       body.classList.add('dark-mode');
       useGlobalStore().setDarkMode(true);
     } else {
       body.classList.add('light-mode');
       useGlobalStore().setDarkMode(false);
     }
+  };
 
-    window
-      ?.matchMedia('(prefers-color-scheme: dark)')
-      .addEventListener('change', e => {
-        body.classList.remove(...themeList);
-        if (e.matches) {
-          body.classList.add('dark-mode');
-          useGlobalStore().setDarkMode(true);
-        } else {
-          body.classList.add('light-mode');
-          useGlobalStore().setDarkMode(false);
-        }
-        console.log('dark mode: ', e.matches);
-      });
-  }
+  const event = e => {
+    body.classList.remove(...themeList);
+    setTheme();
+    console.log('dark mode: ', e.matches);
+  };
+
+  setTheme();
+  if (mql.addEventListener) mql.addEventListener('change', event);
+  // 兼容低版本
+  if (mql.addListener) mql.addListener(event);
 };
