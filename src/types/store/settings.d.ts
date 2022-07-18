a5ded289d446f4572b9cb2dde0a8a8971fd1c53d
ljk <jiakun.ui@gmail.com>
2022-07-18 17:12:19 +0800

    feat(theme-hook): theme settings are now saved in the backend, and separate themes are supported for day and night mode settings

--- src/types/store/settings.d.ts
+++ src/types/store/settings.d.ts
@@ -3,13 +3,23 @@ interface SettingsStoreState {
   githubUser: string;
   syncTime: number;
   theme: {
-    darkMode: boolean;
+    name: CustomTheme;
+    config?: ThemesConfig<name>;
   };
   avatarUrl: string;
   artifactStore: string;
 }
 
 interface SettingsPostData {
-  gistToken: string;
-  githubUser: string;
+  gistToken?: string;
+  githubUser?: string;
+  theme?: {
+    name: CustomTheme;
+    config?: ThemesConfig<name>;
+  };
 }
+
+type CustomTheme = 'light' | 'dark' | 'auto';
+type ThemesConfig<T> = T extends 'auto'
+  ? { dark: CustomTheme; light: CustomTheme }
+  : never;
