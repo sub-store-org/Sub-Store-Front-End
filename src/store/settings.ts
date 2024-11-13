22196612db9c45b9cdb956416dc93afa60a2b9fa
xream <xream@icloud.com>
2024-11-14 00:36:36 +0800

    feat: 悬浮添加按钮开关默认关

--- src/store/settings.ts
+++ src/store/settings.ts
@@ -37,7 +37,7 @@ export const useSettingsStore = defineStore("settingsStore", {
         isEditorCommon: true,
         isSimpleReicon: false,
         showFloatingRefreshButton: false,
-        showFloatingAddButton: true,
+        showFloatingAddButton: false,
         istabBar: false,
         istabBar2: false,
         subProgressStyle: "hidden",
@@ -80,7 +80,7 @@ export const useSettingsStore = defineStore("settingsStore", {
         this.appearanceSetting.isEditorCommon = res.data.data.appearanceSetting?.isEditorCommon ?? true;
         this.appearanceSetting.isSimpleReicon = res.data.data.appearanceSetting?.isSimpleReicon ?? "";
         this.appearanceSetting.showFloatingRefreshButton = res.data.data.appearanceSetting?.showFloatingRefreshButton ?? "";
-        this.appearanceSetting.showFloatingAddButton = res.data.data.appearanceSetting?.showFloatingAddButton ?? true;
+        this.appearanceSetting.showFloatingAddButton = res.data.data.appearanceSetting?.showFloatingAddButton ?? false;
         this.appearanceSetting.istabBar = res.data.data.appearanceSetting?.istabBar ?? "";
         this.appearanceSetting.istabBar2 = res.data.data.appearanceSetting?.istabBar2 ?? "";
         this.appearanceSetting.subProgressStyle = res.data.data.appearanceSetting?.subProgressStyle ?? "hidden";
