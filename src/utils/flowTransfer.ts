f3093cf8e08b7882c3e8a5bd8a57e99a00eaeb75
xream <xream@icloud.com>
2025-07-22 17:39:50 +0800

    feat: 流量四舍五入优化

--- src/utils/flowTransfer.ts
+++ src/utils/flowTransfer.ts
@@ -4,7 +4,7 @@ const flowTransfer = (flow: number, unit: Unit) => {
   const unitList: Unit[] = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
   let unitIndex = unitList.indexOf(unit);
   return (flow < 1024 || unitIndex === unitList.length - 1)
-    ? { value: flow.toFixed(1), unit: unit }
+    ? { value: (Math.round(flow * 100) / 100).toString(), unit: unit }
     : flowTransfer(flow / 1024, unitList[++unitIndex]);
 };
 
